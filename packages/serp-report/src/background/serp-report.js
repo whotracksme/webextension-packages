f037daaa06d23257e515a453603bb1f6c6459d03
Dominik Lubański <dominik.lubanski@gmail.com>
2022-01-26 14:50:35 +0100

    Use tldts from global scope

--- packages/serp-report/src/background/serp-report.js
+++ packages/serp-report/src/background/serp-report.js
@@ -8,11 +8,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0
  */
-import * as tldts from '../../../../tldts/dist/index.esm.min.js';
+
 import {
   WTM_PRIVACY_SCORE_CATEGORIES,
   WTM_PRIVACY_SCORE_COMPRESSED_STATS,
 } from './data.js';
+
+// For now, we assume that "tldts" library is already loaded and exists on the global scope
+const { parse } = globalThis.tldts;
 
 /**
  * Takes a site (e.g. "economist.com") and returns a map of categories
