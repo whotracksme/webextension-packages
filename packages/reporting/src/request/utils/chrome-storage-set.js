c442f908966f5d5c630f6abeffedabc2bc111ad8
Krzysztof Modras <chrmod@chrmod.net>
2023-08-28 13:07:06 +0200

    General purpose stored Map and Set

--- packages/reporting/src/request/utils/chrome-storage-set.js
+++ packages/reporting/src/request/utils/chrome-storage-set.js
@@ -12,41 +12,26 @@
 
 import ChromeStorageBase from './chrome-storage-base';
 
-// This global provides an API like an ES Map but will sync
-// with local storage from time to time. That is done to prevent
-// the loss of all stats when the browser terminates the execution
-// context (background script or service worker).
-export default class ChromeStorageMap extends ChromeStorageBase {
+export default class ChromeStorageSet extends ChromeStorageBase {
   constructor(options) {
     super(options);
-    this.inMemoryData = new Map();
+    this.inMemoryData = new Set();
   }
 
   deserialise(entries) {
-    return new Map(Object.entries(entries));
+    return new Set(entries);
   }
 
   serialise(entries) {
-    return Object.fromEntries(entries);
+    return [...entries];
   }
 
-  get(_key) {
-    this._warnIfOutOfSync();
-    const key = this.normalizeKey(_key);
-    return this.inMemoryData.get(key);
-  }
-
-  forEach(callback) {
-    this._warnIfOutOfSync();
-    this.inMemoryData.forEach(callback);
-  }
-
-  set(_key, value) {
+  add(_value) {
     this._warnIfOutOfSync();
 
-    // This should never trigger. Yet if the maps run full (perhaps
+    // This should never trigger. Yet if the sets run full (perhaps
     // as a side-effect of a bug), better reset then continuing with
-    // these huge maps.
+    // these huge sets.
     if (
       this.inMemoryData.size >= this.maxEntries ||
       this._ttlMap.size >= this.maxEntries
@@ -57,18 +42,24 @@ export default class ChromeStorageMap extends ChromeStorageBase {
       this.inMemoryData.clear();
       this._ttlMap.clear();
     }
-    const key = this.normalizeKey(_key);
-    this.inMemoryData.set(key, value);
-    this._ttlMap.set(key, Date.now() + this.ttlInMs);
+    const value = this.normalizeKey(_value);
+    this.inMemoryData.add(value);
+    this._ttlMap.set(value, Date.now() + this.ttlInMs);
     this._markAsDirty();
   }
 
-  delete(_key) {
+  has(_value) {
     this._warnIfOutOfSync();
-    const key = this.normalizeKey(_key);
-    const wasDeleted = this.inMemoryData.delete(key);
+    const value = this.normalizeKey(_value);
+    return this.inMemoryData.has(value);
+  }
+
+  delete(_value) {
+    this._warnIfOutOfSync();
+    const value = this.normalizeKey(_value);
+    const wasDeleted = this.inMemoryData.delete(value);
     if (wasDeleted) {
-      this._ttlMap.delete(key);
+      this._ttlMap.delete(value);
       this._markAsDirty();
     }
     return wasDeleted;
---
+++ packages/reporting/test/request/utils/chrome-storage-map.spec.js
@@ -0,0 +1,26 @@
+/**
+ * WhoTracks.Me
+ * https://whotracks.me/
+ *
+ * Copyright 2017-present Ghostery GmbH. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0
+ */
+
+import * as chai from 'chai';
+
+import ChromeStorageMap from '../../../src/request/utils/chrome-storage-map';
+
+describe('utils/chrome-storage-map', function () {
+  it('set / get', function () {
+    const map = new ChromeStorageMap({
+      storageKey: 'test',
+    });
+    const key = 1;
+    const value = 2;
+    map.set(key, value);
+    chai.expect(map.get(key)).to.equal(value);
+  });
+});
