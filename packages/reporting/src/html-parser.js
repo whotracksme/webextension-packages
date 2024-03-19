b0e433bec3cb9ea5fc7d4506ad409e23a4bd4be7
Philipp Claßen <philipp@ghostery.com>
2024-03-19 18:36:45 +0100

    Enable linkedom caching.

--- packages/reporting/src/html-parser.js
+++ packages/reporting/src/html-parser.js
@@ -9,7 +9,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0
  */
 
-import { DOMParser } from 'linkedom';
+import { DOMParser } from 'linkedom/cached';
 
 export default function parseHtml(html) {
   const domParser = new DOMParser();
