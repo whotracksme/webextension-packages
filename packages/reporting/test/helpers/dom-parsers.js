555c58610d60495e33a6d614f516b8ae7bd4a611
Philipp Claßen <philipp@ghostery.com>
2024-03-19 18:36:45 +0100

    Enable linkedom caching.

--- packages/reporting/test/helpers/dom-parsers.js
+++ packages/reporting/test/helpers/dom-parsers.js
@@ -9,7 +9,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0
  */
 
-import * as linkedom from 'linkedom';
+import * as linkedom from 'linkedom/cached';
 
 /**
  * Different implementations of the DOM API.
