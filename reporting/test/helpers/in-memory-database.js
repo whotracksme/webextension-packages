f51b121edcf3a4b422ccee3cfdc495e1e3a87c8c
Krzysztof Modras <chrmod@chrmod.net>
2024-07-08 12:04:55 +0200

    Fix tests

--- reporting/test/helpers/in-memory-database.js
+++ reporting/test/helpers/in-memory-database.js
@@ -9,6 +9,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0
  */
 
-import InMemoryDatabase from '../../../anonymous-communication/test/helpers/in-memory-database.js';
+import InMemoryDatabase from '../../../communication/test/helpers/in-memory-database.js';
 
 export default InMemoryDatabase;
